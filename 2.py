6fb5a324288a66dd960fe71e998ebf7a3088b45c
KonTroAl <kostya98295@mail.ru>
2020-05-26 22:48:11 +0500

    Complit Task_2

---
+++ 2.py
@@ -0,0 +1,9 @@
+with open("text_2.txt", "r", encoding="utf-8") as my_file:
+
+    content = my_file.readlines()
+    print(content)
+    print("Количество строк в Вашем файле: ", len(content))
+
+    for i in content:
+        n = i.split()
+        print(f"В строке {i} содержится {len(n)} слов!", end="\n")
